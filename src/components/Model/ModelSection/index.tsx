48eaa3363fb159b0f4fec393b11881ee69b3486d
Viniciuslb <viniciuslb067@gmail.com>
2021-05-29 11:16:13 -0300

    feat: crud

--- src/components/Model/ModelSection/index.tsx
+++ src/components/Model/ModelSection/index.tsx
@@ -1,19 +1,33 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef } from "react";
+import useModel from "../useModel";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   modelName: string;
-  children:ReactNode;
+  children: ReactNode;
   overlayNode: ReactNode;
 }
 
 function ModelSection({ modelName, overlayNode, children, ...props }: Props) {
+  const { registerModel } = useModel(modelName);
+
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (sectionRef.current)
+      registerModel({
+        modelName,
+        overlayNode,
+        sectionRef,
+      });
+  }, []);
+
   return (
-    <Container {...props}>
+    <Container ref={sectionRef} {...props}>
       {children}
     </Container>
   );
-};
+}
 
 export default ModelSection;
